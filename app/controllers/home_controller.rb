57630600459014d0dc8267346aa7b4542ca64701
Gustavo Guichard <gustavoguichard@gmail.com>
2013-07-18 20:26:03 -0300

    Fixes a bug when homepage displayed draft projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # Highlighted projects
     # TODO @GG add correct filter here!
-    @projects = Project.limit(4)
+    @projects = Project.visible.limit(4)
     @donate_banner_projects = Project.by_kind(:donate).limit(3)
     @invest_banner_projects = Project.by_kind(:invest).limit(3)
   end
