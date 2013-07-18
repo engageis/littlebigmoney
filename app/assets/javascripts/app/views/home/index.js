3785d79b23db79d2faf01545b6994bcc13a9b996
Gustavo Guichard <gustavoguichard@gmail.com>
2013-07-18 19:41:52 -0300

    Javascript for the project banner in Home page

--- app/assets/javascripts/app/views/home/index.js
+++ app/assets/javascripts/app/views/home/index.js
@@ -1,8 +1,23 @@
 CATARSE.HomeIndexView = Backbone.View.extend({
+  events: {
+    'click .banner_project a': 'changeBannerProject',
+  },
+
   initialize: function() {
     _.bindAll(this, "startSlider")
     this.startSlider();
+  },
 
+  changeBannerProject: function(event) {
+    var $parentSlide = $(event.target).closest('.slide');
+    $featuredBanner = $('.featured_banner_project', $parentSlide);
+    var $project = $(event.target).closest('.banner_project');
+    var projectImage = $('img', $project).attr('src');
+    $('.banner_project', $parentSlide).removeClass('active');
+    $project.addClass('active');
+    $('.proj_img img', $featuredBanner).attr('src', projectImage);
+    $('.proj_title', $featuredBanner).html('<a href="'+$project.data('link')+'">'+$project.data('name')+'</a>');
+    $('.proj_desc', $featuredBanner).html($project.data('description'));
   },
 
   startSlider: function(event) {
